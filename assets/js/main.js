98af8b750c2e97ed41018e220496d344e20ec919
Nizar Khan <131904091+NizarKhan0@users.noreply.github.com>
2023-05-25 16:01:48 +0800

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,77 @@
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+
+toggleButton.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+});
+
+//Reviews Swiper
+var swiper = new Swiper(".reviews-content", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+//Email JS
+function Validate() {
+  let name = document.querySelector(".name");
+  let email = document.querySelector(".email");
+  let msg = document.querySelector(".message");
+  let sendBtn = document.querySelector(".send-btn");
+
+  sendBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (name.value == "" || email.value == "" || msg.value == "") {
+      emptyerror();
+    } else {
+      sendmail(name.value, email.value, msg.value);
+      success();
+    }
+  });
+}
+Validate();
+
+function sendmail(name, email, msg) {
+  emailjs.send("service_9hdtfbk", "template_9oqw406", {
+    to_name: email,
+    from_name: name,
+    message: msg,
+  });
+}
+
+function emptyerror() {
+  swal({
+    title: "Oh No....!",
+    text: "Fields cannot be empty!",
+    icon: "error",
+  });
+}
+
+function success() {
+  swal({
+    title: "Email sent successfully",
+    text: "We try to reply in 24 hours",
+    icon: "success",
+  });
+}
+
+//Header Background Change on Scroll
+let header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+  header.classList.toggle("header-active", window.scrollY > 0);
+});
+
+//Scroll Top
+let scrollTop = document.querySelector(".scroll-top");
+
+window.addEventListener("scroll", () => {
+  scrollTop.classList.toggle("scroll-active", window.scrollY >= 400);
+});
